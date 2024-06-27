f617db91a8afe9c44cf5809b5ed7efd3502479c4
ysicy <alexeyecko@gmail.com>
2024-06-27 19:11:09 +0300

    xxx cyka!!!!

--- src/test/java/tests/web/TestBaseUi.java
+++ src/test/java/tests/web/TestBaseUi.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
 import java.util.Map;
 
 
@@ -15,13 +16,15 @@ public class TestBaseUi {
     @BeforeAll
     static void beforeAll(){
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = false;
-        Configuration.remote ="https://user1:1234@Selenoid.autotests.cloud/wo/hub";
+        Configuration.browserSize="1020x800";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
-        "enableVideo", true));
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
