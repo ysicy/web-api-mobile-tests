4e1e17e2f0f8c24d9d7010aa69672cf87a61b87f
Alex Ecko <alexeyecko@gmail.com>
2024-07-10 15:16:31 +0300

    подкинул свой selenoid

--- src/test/java/tests/web/BigencUiTests.java
+++ src/test/java/tests/web/BigencUiTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import pages.BigencPage;
-import tests.annotation.BrowserType;
+
 
 
 import static com.codeborne.selenide.Selenide.open;
